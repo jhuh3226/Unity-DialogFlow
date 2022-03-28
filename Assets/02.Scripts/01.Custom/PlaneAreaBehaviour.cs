42b8ff993c223d7dab1bbae842da9334045bcc38
jhuh3226 <27257844+jhuh3226@users.noreply.github.com>
2022-03-28 06:51:10 -0400

    Added ar remote

--- Assets/02.Scripts/01.Custom/PlaneAreaBehaviour.cs
+++ Assets/02.Scripts/01.Custom/PlaneAreaBehaviour.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using TMPro;
 
 [RequireComponent (typeof (ARRaycastManager), typeof (AudioSource), typeof (ARPlaneManager))]
 public class PlaneAreaBehaviour : MonoBehaviour {
@@ -12,17 +12,16 @@ public class PlaneAreaBehaviour : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start () {
-
+        Debug.Log ("PlaneAreaBehaviour initialized");
     }
 
-    // Update is called once per frame
-    void Update () {
-        areaText.transform.rotation =
-            Quaternion.LookRotation (areaText.transform.position -
-                Camera.main.transform.position);
+    private void Update () {
+        // Set the areaText gameobject transform to always look at the MainCamera
+        areaText.transform.rotation = Quaternion.LookRotation (areaText.transform.position - Camera.main.transform.position);
     }
 
     private void ArPlane_BoundaryChanged (ARPlaneBoundaryChangedEventArgs obj) {
+        Debug.Log ("ArPlane_BoundaryChanged");
         areaText.text = CalculatePlaneArea (arPlane).ToString ();
     }
     private float CalculatePlaneArea (ARPlane plane) {
@@ -30,9 +29,16 @@ public class PlaneAreaBehaviour : MonoBehaviour {
     }
 
     public void ToggleAreaView () {
-        if (areaText.enabled)
-            areaText.enabled = false;
-        else
-            areaText.enabled = true;
+        areaText.enabled = true;
+
+        // if (areaText.enabled) {
+        //     areaText.enabled = false;
+        // } else
+        //     areaText.enabled = true;
+    }
+
+    public void ArPlane_AskCalculation () {
+        Debug.Log ("Ask for calculation");
+        areaText.text = CalculatePlaneArea (arPlane).ToString ();
     }
 }
--- Assets/02.Scripts/01.Custom/PlaneAreaManager.cs
+++ Assets/02.Scripts/01.Custom/PlaneAreaManager.cs
@@ -18,11 +18,15 @@ public class PlaneAreaManager : MonoBehaviour {
             var touch = Input.GetTouch (0);
             if (touch.phase == TouchPhase.Ended) {
                 if (Input.touchCount == 1) {
+                    Debug.Log ("touch count 1");
                     Ray raycast = Camera.main.ScreenPointToRay (touch.position);
                     if (Physics.Raycast (raycast, out RaycastHit raycastHit)) {
-                        var planeAreaBehaviour =
-                            raycastHit.collider.gameObject.GetComponent<PlaneAreaBehaviour> ();
+                        Debug.Log ("raycast hit");
+                        var planeAreaBehaviour = raycastHit.collider.gameObject.GetComponent<PlaneAreaBehaviour> ();
+                        // Debug.Log (planeAreaBehaviour);
                         if (planeAreaBehaviour != null) {
+                            Debug.Log ("communicate with PlaneAreaBehaviour");
+                            planeAreaBehaviour.ArPlane_AskCalculation();
                             planeAreaBehaviour.ToggleAreaView ();
                         }
                     }
